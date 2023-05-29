a6a56ef1f0b94527751f7bb4e307665a08db1e74
Peter Bui <pbui27@uw.edu>
2023-05-28 19:53:28 -0700

    test

---
+++ _webapp/model.py
@@ -0,0 +1,46 @@
+import os
+import streamlit as st
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+
+# Load the model
+path = os.path.dirname(__file__)
+device = torch.device('cpu')
+
+@st.cache(hash_funcs={"MyUnhashableClass": lambda _: None})
+def load_model():
+    model = torchvision.models.efficientnet_v2_l(weights='EfficientNet_V2_L_Weights.DEFAULT')
+    num_features = model.classifier[1].in_features 
+    model.classifier[1] = nn.Linear(num_features, 555)
+    my_file = path + '/model_checkpoint_effnetv2.pt'
+    checkpoint = torch.load(my_file, map_location=device)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    model.eval()
+    return model
+
+model = load_model()
+
+# Define transformations
+transformations = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+# Load bird names into a list
+my_file = path + '/names.txt'
+bird_names = [line.strip() for line in open(my_file)]
+
+# Predicts the top 3 birds from an image
+def predict(image):
+    image_tensor = torch.unsqueeze(transformations(image), 0)
+    image_tensor = image_tensor.to(device)
+
+    output = model(image_tensor)
+
+    preds = torch.nn.functional.softmax(output, dim=1)[0]
+    _, indices = torch.sort(output, descending=True)
+    return [(bird_names[i], preds[i].item()) for i in indices[0][:3]]
